ea5e1f7eb4754b1595f3d9e6cf4c0c67be6300fd
frankwangws <frank.wang.w.s@gmail.com>
2019-04-14 21:21:23 +0800

    Update instructions and optimizations

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 # MPSDiffCircuit.jl
 Quick realization of MPS-differentiable-qubit circuit. 
+
+# Installation
+in Julia Package mode please type:
+```
+pkg> add https://github.com/frankwswang/MPSDiffCircuit.jl.git
+```
+
+For compatibility please also install dependent package:
+```
+pkg> add Yao
+pkg> add https://github.com/QuantumBFS/QuAlgorithmZoo.jl.git
+```
--- src/MPSDiffCircuit.jl
+++ src/MPSDiffCircuit.jl
@@ -1,23 +1,30 @@
 module MPSDiffCircuit
 
+"""
+A quick realization of MPS-differentiable-qubit circuit. 
+
+For compatibility please also install in Julia Package mode:
+add Yao
+add https://github.com/QuantumBFS/QuAlgorithmZoo.jl.git 
+"""
 using Yao, Yao.Blocks
 using QuAlgorithmZoo
 
 export MPSDC
 
 struct MPSDC
-    circuit
-    cBlocks
-    cExpend
-    cEBlocks
-    diffs
-    nBit
+    circuit  # MPS differentiable circuit
+    cBlocks  # Array of all the MPS blocks in MPS circuit.
+    cExtend  # The circuit after extending the MPS circuit.
+    cEBlocks # Array of all the MPS blocks in extended circuit.
+    diffs    # differentials of MPS circuit.
+    nBit     # Number of lines(bits) of MPS circuit. 
     
     function MPSDC(depth::Int64, nBitA::Int64, vBit::Int64, rBit::Int64=1)
         (nBitA - vBit -rBit) % rBit != 0 && println("Error: Nblock is not integer!")
         Nblock = Int((nBitA - vBit) / rBit)
         nBit = rBit + vBit
-        cBlock = random_diff_circuit(nBit, depth, pair_ring(nBit)) 
+        cBlock = random_diff_circuit(nBit, depth, pair_ring(nBit)) |> autodiff(:QC)
         cBlocks = []
         cEBlocks = []
         for i = 1:Nblock
@@ -25,10 +32,10 @@ struct MPSDC
             cEBlocks = push!(cEBlocks, put( nBitA, Tuple((nBitA-rBit*(i-1)):-1:(nBitA-rBit*i-vBit+1),)=>cBlock) )
         end
         circuit = chain(nBit,cBlocks)
-        cExpand = chain(nBitA, cEBlocks) |> autodiff(:QC)
-        circuit = dispatch!(circuit, :random) |> autodiff(:QC)
+        cExtend = chain(nBitA, cEBlocks) 
+        circuit = dispatch!(circuit, :random)
         diffs = collect(circuit, AbstractDiff)
-        new(circuit, cBlocks, cExpand, cEBlocks, diffs, nBit)
+        new(circuit, cBlocks, cExtend, cEBlocks, diffs, nBit)
     end
 end
 
