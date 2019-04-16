127858821207d719309af08b69818c29a18edfc3
frankwangws <frank.wang.w.s@gmail.com>
2019-04-16 12:50:10 +0800

    Adding a field of the struct.

--- src/MPSDiffCircuit.jl
+++ src/MPSDiffCircuit.jl
@@ -16,27 +16,28 @@ export MPSDC
 struct MPSDC
     circuit  # MPS differentiable circuit
     cBlocks  # Array of all the MPS blocks in MPS circuit.
-    cExtend  # The circuit after extending the MPS circuit.
+    cExtend  # MPS circuit extended back to where it doesn't reuse any qubit.
     cEBlocks # Array of all the MPS blocks in extended circuit.
     diffs    # differentials of MPS circuit.
     nBit     # Number of lines(bits) of MPS circuit. 
+    nBlock   # Number of blocks in MPS ciruict.
     
     function MPSDC(depth::Int64, nBitA::Int64, vBit::Int64, rBit::Int64=1)
-        (nBitA - vBit -rBit) % rBit != 0 && println("Error: Nblock is not integer!")
-        Nblock = Int((nBitA - vBit) / rBit)
+        (nBitA - vBit -rBit) % rBit != 0 && println("Error: nBlock is not integer!")
+        nBlock = Int((nBitA - vBit) / rBit)
         nBit = rBit + vBit
         cBlock = random_diff_circuit(nBit, depth, pair_ring(nBit)) |> autodiff(:QC)
         cBlocks = []
         cEBlocks = []
-        for i = 1:Nblock
+        for i = 1:nBlock
             cBlocks = push!(cBlocks, cBlock)
-            cEBlocks = push!(cEBlocks, put( nBitA, Tuple((nBitA-rBit*(i-1)):-1:(nBitA-rBit*i-vBit+1),)=>cBlock) )
+            cEBlocks = push!(cEBlocks, put( nBitA, Tuple((nBitA-rBit*i-vBit+1):(nBitA-rBit*(i-1)),)=>cBlock) )
         end
         circuit = chain(nBit,cBlocks)
         cExtend = chain(nBitA, cEBlocks) 
         circuit = dispatch!(circuit, :random)
         diffs = collect(circuit, AbstractDiff)
-        new(circuit, cBlocks, cExtend, cEBlocks, diffs, nBit)
+        new(circuit, cBlocks, cExtend, cEBlocks, diffs, nBit, nBlock)
     end
 end
 
